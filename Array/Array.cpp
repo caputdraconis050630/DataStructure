dfca14b52e593e8b7793acdc4e42e4f0c5776a0f
caputdraconis <60993104+caputdraconis050630@users.noreply.github.com>
2021-04-08 17:13:04 +0900

    Array.cpp
    Week Practice Code

---
+++ Array/Array.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+class Array{
+private:
+    int *arr;
+    int arrSize;
+    
+public:
+    Array(int sz){
+        this->arrSize = sz;
+        this->arr = new int[arrSize];
+        for(int i=0;i<arrSize;i++){
+            arr[i] = 0;
+        }
+        
+    }
+    
+    int at(int idx){
+        return arr[idx];
+    }
+    
+    void add(int idx, int value){
+        for(int i = arrSize-2;i >= idx; i++){
+            arr[i+1] = arr[i]; // idx 인덱스의 값부터 한칸씩 오른쪽으로 이동
+        }
+        arr[idx] = value;
+    }
+    
+    void set(int idx, int value){
+        arr[idx] = value;
+    }
+    
+    void print(){
+        for(int i=0;i<arrSize;i++){
+            cout<<arr[i]<<" ";
+        }
+        cout<<endl;
+    }
+    
+    void remove(int idx){
+        for(int i = idx;i<arrSize-1;i++){
+            arr[i] = arr[i+1];
+        }
+        arr[arrSize-1] = 0;
+    }
+};
